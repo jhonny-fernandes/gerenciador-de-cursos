12a2b1115c8b0a2c33e8491e08f4daa903fab3cb
JonnyFernandes <jonasdossantos647@gmail.com>
2024-02-01 12:01:00 -0300

    a

--- create.php
+++ create.php
@@ -7,20 +7,25 @@ $estado    = filter_var($_POST['estado'], FILTER_SANITIZE_SPECIAL_CHARS);
 $preco     = filter_var($_POST['preco'], FILTER_SANITIZE_NUMBER_INT);
 $descricao = filter_var($_POST['descricao'], FILTER_SANITIZE_SPECIAL_CHARS);
 
-// $verifyNameExist = "SELECT *FROM produtos WHERE nome=$nome LIMIT 1";
-// $verifyNameExistExec =$conexao->query($verifyNameExist);
-// var_dump($verifyNameExistExec);
-
 $dirUpload = "upload/";
 
 $extensaoArquivo = strrchr($_FILES['arquivo']['name'], '.');             // FAZENDO UMA BUSCA PELA EXTENSÃO
 $novoNomeArquivo = md5($_FILES['arquivo']['name']).$extensaoArquivo;     // Gera novo nome para o arquivo
 
+$verifyName = "SELECT COUNT(*) as total FROM produtos WHERE nome = 'Elementor Builder'";
+$verifyNameExec = $conexao->query($verifyName);
 
-if(!is_dir($dirUpload)){    
-    echo "<p class='alert alert-danger'> Diretório não encontrado, falha no upload </p>";
+if ($verifyNameExec) {
+    $result = $verifyNameExec->fetch(PDO::FETCH_ASSOC);
+    $total = $result['total'];
+
+    if ($total >= 1) {
+        echo "<p class='alert alert-danger'> Uuups, o curso já existe em </p>";
+    } else {
+       if(!is_dir($dirUpload)){    
+            echo "<p class='alert alert-danger'> Diretório não encontrado, falha no upload </p>";
   
-}  else{
+    }  else{
     
     if( $extensaoArquivo != '.jpg' && $extensaoArquivo != '.png') {
         echo "<p class='alert alert-danger'> Extensão não aceita, aceitamos: <strong> PNG, JPG </strong> </p>";
@@ -48,4 +53,44 @@ if(!is_dir($dirUpload)){
     }
     
 }  
+    }
+}
+
+// $dirUpload = "upload/";
+
+// $extensaoArquivo = strrchr($_FILES['arquivo']['name'], '.');             // FAZENDO UMA BUSCA PELA EXTENSÃO
+// $novoNomeArquivo = md5($_FILES['arquivo']['name']).$extensaoArquivo;     // Gera novo nome para o arquivo
+
+
+// if(!is_dir($dirUpload)){    
+//     echo "<p class='alert alert-danger'> Diretório não encontrado, falha no upload </p>";
+  
+// }  else{
+    
+//     if( $extensaoArquivo != '.jpg' && $extensaoArquivo != '.png') {
+//         echo "<p class='alert alert-danger'> Extensão não aceita, aceitamos: <strong> PNG, JPG </strong> </p>";
+//     }else{
+//         $nameFileServer = $dirUpload.$novoNomeArquivo;
+//         move_uploaded_file($_FILES['arquivo']['tmp_name'], $nameFileServer);
+
+//         /* -- INSERT -- */
+//         $sqlInsert = "INSERT INTO produtos 
+//         (nome, descricao, arquivo, preco, estado, criado_em, atualizado_em)
+//         VALUES(:nome, :descricao, :arquivo, :preco, :estado, NOW(), NOW())";
+
+//             // LIMPANDO SUJEIRA DE DADOS
+//         $insert = $conexao->prepare($sqlInsert);
+//         $insert->bindValue(':nome', $nome, PDO::PARAM_STR);
+//         $insert->bindValue(':arquivo', $dirUpload.$novoNomeArquivo, PDO::PARAM_STR);
+//         $insert->bindValue(':descricao',  $descricao, PDO::PARAM_STR);
+//         $insert->bindValue(':preco',  $preco, PDO::PARAM_STR);
+//         $insert->bindValue(':estado', $estado, PDO::PARAM_STR);
+
+//         $insert->execute();
+
+//         echo "<p class='alert alert-success'> Cadastro realizado! </p>";
+        
+//     }
+    
+// }  
     
--- update.php
+++ update.php
@@ -13,38 +13,48 @@ $descricao = filter_var($_POST['descricao'], FILTER_SANITIZE_SPECIAL_CHARS);
 $dirUpload = "upload/";
 
 if(!empty($_FILES['arquivo'])){
-    // SE NÃO ESTIVER VAZIA
+    // SE NÃO ESTIVER VAZIA,CRIE O ARQUIVO
 
     $extensaoArquivo = strrchr($_FILES['arquivo']['name'], '.');             // FAZENDO UMA BUSCA PELA EXTENSÃO
-    $novoNomeArquivo = md5($_FILES['arquivo']['name']).$extensaoArquivo;     // Gera novo nome para o arquivo
 
-    $nameFileServer = $dirUpload.$novoNomeArquivo;
-    move_uploaded_file($_FILES['arquivo']['tmp_name'], $nameFileServer);
-
-    
-        $sqlUpdate = "UPDATE produtos 
-                         SET nome= :nome, 
-                             arquivo= :arquivo, 
-                             estado=:estado,
-                             preco= :preco, 
-                             descricao= :descricao 
-                       WHERE id =:id";
-
-        $update = $conexao->prepare($sqlUpdate);
-
-        $update->bindValue(':id', $id);
-        $update->bindValue(':nome', $nome);
-        $update->bindValue(':arquivo', $dirUpload.$novoNomeArquivo);
-        $update->bindValue(':estado', $estado);
-        $update->bindValue(':preco', $preco);
-        $update->bindValue(':descricao', $descricao);
+    if(empty($extensaoArquivo)){
+        echo "sem extensão";
         
+        return false;
+   }    else{
+        $novoNomeArquivo = md5($_FILES['arquivo']['name']).$extensaoArquivo;     // Gera novo nome para o arquivo
 
-        $update->execute();
+        $nameFileServer = $dirUpload.$novoNomeArquivo;
+        move_uploaded_file($_FILES['arquivo']['tmp_name'], $nameFileServer);
 
-    echo "<p class='alert alert-success'> Produto atualizado com sucesso! </p>";
+        
+            $sqlUpdate = "UPDATE produtos 
+                            SET nome= :nome, 
+                                arquivo= :arquivo, 
+                                estado=:estado,
+                                preco= :preco, 
+                                descricao= :descricao 
+                        WHERE id =:id";
+
+            $update = $conexao->prepare($sqlUpdate);
+
+            $update->bindValue(':id', $id);
+            $update->bindValue(':nome', $nome);
+            $update->bindValue(':arquivo', $dirUpload.$novoNomeArquivo);
+            $update->bindValue(':estado', $estado);
+            $update->bindValue(':preco', $preco);
+            $update->bindValue(':descricao', $descricao);
+            
+            $update->execute();
+
+        echo "<p class='alert alert-success'> Produto atualizado com sucesso! </p>";
+        return true;
+   }
+   
 }   else {
+                   
        /* -- UPDATE -- */
+       
        $sqlUpdate = "UPDATE produtos 
                         SET nome= :nome, 
                             estado=:estado,
